5fd3fdf5b7bc175b0d56841ae0be75401052720a
tmzmfldkqls <33713889+tmzmfldkqls@users.noreply.github.com>
2017-11-17 13:40:26 +0900

    기존 내용 삭제 + 함수추가해ㅐㅆ음

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -1,12 +1,20 @@
 package test;
 
 public class hello {
-
+	
+	public static String print(String str) {
+		
+		String str1 = str + "can write anything";
+		
+		return str1;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("¾È³ç");
-		System.out.println("dl");
 		System.out.println("rrww");
+		
+		System.out.println(print("team"));
 
 	}
-
+	
 }
