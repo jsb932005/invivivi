827ef3e5b835028b72909dbed99ca3102c2200c8
tmzmfldkqls <33713889+tmzmfldkqls@users.noreply.github.com>
2017-11-17 14:14:11 +0900

    마스터-기본

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -1,20 +1,17 @@
 package test;
 
 public class hello {
-	
-	public static String print(String str) {
-		
-		String str1 = str + "can write anything";
-		
-		return str1;
+
+	public static String prt(String str) {
+		String str2;
+		str2 = str + " can write anything, anytime!!";
+		return str2;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("¾È³ç");
-		System.out.println("rrww");
-		
-		System.out.println(print("team"));
-
+		System.out.println("dl");
+		prt("team");
 	}
-	
+
 }
